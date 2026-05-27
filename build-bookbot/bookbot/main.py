574875d879e223b8cfec94cef8324ed340ce0fe1
Sameer Jain <sameerjain501@gmail.com>
2026-05-27 14:36:41 -0400

    status update

--- build-bookbot/bookbot/main.py
+++ build-bookbot/bookbot/main.py
@@ -1,17 +1,24 @@
-from stats import get_num_words, count_letters
+from stats import *
 
-def print_num_words(num_words: list[str])-> str:
+def print_num_words(num_words: int)-> str:
     return print(f"Found {num_words} total words")
 
-def print_freq_letters(freq_letters: dict[str,int])->str:
-    
+def print_freq_letters(freq_letters: dict[str,int])->None:
+    for key,value in freq_letters.items():
+        print(f"'{key}': value")
+
 
 def main():
 
     # Read File Challenge
-    num_words = get_num_words("books/frankenstein.txt")
-    print_num_words(num_words)
+    num_words = get_num_words(
+        convert_book_to_list(get_book_txt("books/frankenstein.txt"))
+    )
+    print_num_words(
+        get_num_words(convert_book_to_list(get_book_txt("books/frankenstein.txt")))
+    )
 
+    freq_letters = get_freq_letters()
 
 if __name__ == "__main__":
     main()
--- build-bookbot/bookbot/stats.py
+++ build-bookbot/bookbot/stats.py
@@ -3,13 +3,12 @@ def get_book_txt(book_path: str) -> str:
         file_contents = f.read()
     return file_contents
 
-def convert_book_to_list(book_path:str) -> list[str]:
-    word_list = get_book_txt(book_path).split()
+def convert_book_to_list(file_contents:str) -> list[str]:
+    word_list = file_contents.split()
     return word_list
 
 def get_freq_letters(word_list:list[str]) -> dict[str,int]:
     pass
 
-def get_num_words(book_path: str) -> int:
-    num_words = convert_book_to_list(book_path) 
-    return len(num_words)
+def get_num_words(word_list: list[str]) -> int:
+    return len(word_list)
