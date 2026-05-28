47fab1af971947a6d58896ad15c5105cd65e97d0
Sameer Jain <sameerjain501@gmail.com>
2026-05-27 23:48:06 -0400

    testing gitignore

--- build-bookbot/bookbot/main.py
+++ build-bookbot/bookbot/main.py
@@ -1,32 +1,46 @@
 import sys
+import os
 from stats import *
 from prints import * 
 
+def print_books_dir() -> None:
+    pass
 
 def main() -> None:
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         return sys.exit(1)
     
+    print("Welcome to the Samatron ""Bookbot""!\nAll books are grabbed from the /books folder\nSelect which book (.txt file) you would like to get stats of:")
+    
+    books_list = os.listdir("books")
+    books_menu = {}
+    for i in range(0,len(books_list)):
+        books_menu[i+1] = books_list[i]
+    for key,value in books_menu.items():
+        print(f"{key}: {value}")
+        
+
     book_path = sys.argv[1]
-    generate_report(book_path)
+    print_report(book_path)
+
+    print()
 
 
 if __name__ == "__main__":
     main()
 
-    """
-    MENU TEXT FOR LATER 
-    "Welcome to the Samatron "Bookbot"!\nAll books are grabbed from the /books folder\nSelect which book (.txt file) you would like to get stats of:" 
+    # 
 
-    0: Recheck the /books folder 
-    QUIT
-    "Now enter which stat you would like:"
-    Total amount of words
-    Total amount of characters
-    Frequency of each word 
-    Frequency of each character
+    # 0: Recheck the /books folder 
+    # QUIT
+    # "Now enter which stat you would like:"
+    # Total amount of words
+    # Total amount of characters
+    # Frequency of each word 
+    # Frequency of each character
+      Get the standard report
 
-    Select a different book
-    QUIT
-    """
+    # Select a different book
+    # QUIT
+    # """
--- build-bookbot/bookbot/prints.py
+++ build-bookbot/bookbot/prints.py
@@ -5,7 +5,7 @@ class CharacterCount(TypedDict):
     char: str
     num: int
 
-def generate_report(book_path: str):
+def print_report(book_path: str) -> None:
     file_contents = get_book_txt(book_path)
     word_list = get_word_list(file_contents)
     char_freqs = get_char_freqs_iter(file_contents)
@@ -19,19 +19,19 @@ def generate_report(book_path: str):
     print_sorted_dict_list(sorted_char_freqs)
     print("============ END ============")
 
-def sort_key(CharacterCount):
+def sort_key(CharacterCount) -> int:
     return CharacterCount["num"]
 
-def create_sorted_dict_list(char_freqs:dict[str,int])-> list[CharacterCount]:
+def create_sorted_dict_list(char_freqs:dict[str,int]) -> list[CharacterCount]:
     sorted_dict_list = []
-    
+
     for key,value in char_freqs.items():
         char_dict:CharacterCount = {"char": key,"num":value}
         sorted_dict_list.append(char_dict)
     sorted_dict_list.sort(reverse=True,key=sort_key)
     return sorted_dict_list
-    
-def print_sorted_dict_list(sorted_dict_list: list[CharacterCount])->None:
+
+def print_sorted_dict_list(sorted_dict_list: list[CharacterCount]) -> None:
     for char_dict in sorted_dict_list:
         if char_dict["char"].isalpha():
             print(f"{char_dict["char"]}: {char_dict["num"]}")
