623cdaba8e690f8a706f511a1af8247808e847ab
Sameer Jain <sameerjain501@gmail.com>
2026-05-28 00:19:54 -0400

    more menu prints

--- build-bookbot/bookbot/main.py
+++ build-bookbot/bookbot/main.py
@@ -6,41 +6,38 @@ from prints import *
 def print_books_dir() -> None:
     pass
 
+def print_dict
+
 def main() -> None:
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 1:
         print("Usage: python3 main.py <path_to_book>")
         return sys.exit(1)
-    
+
     print("Welcome to the Samatron ""Bookbot""!\nAll books are grabbed from the /books folder\nSelect which book (.txt file) you would like to get stats of:")
-    
+
     books_list = os.listdir("books")
     books_menu = {}
     for i in range(0,len(books_list)):
-        books_menu[i+1] = books_list[i]
+        books_menu[str(i+1)] = books_list[i]
+    books_menu{"0":"QUIT"}
     for key,value in books_menu.items():
         print(f"{key}: {value}")
-        
 
-    book_path = sys.argv[1]
+    value = input("Enter the corresponding Value")
+
+    print("Now enter which stat you would like:")
+    options = {"1": "Total amount of words",
+               "2":"Total amount of characters",
+               "3":"Frequency of each word",
+               "4":"Get the standard report",
+               "0": "QUIT"}
+
+    book_path = "books/" + books_menu[value]
     print_report(book_path)
 
-    print()
+    print("Would you like to try another book or quit")
 
 
 if __name__ == "__main__":
     main()
 
-    # 
-
-    # 0: Recheck the /books folder 
-    # QUIT
-    # "Now enter which stat you would like:"
-    # Total amount of words
-    # Total amount of characters
-    # Frequency of each word 
-    # Frequency of each character
-      Get the standard report
-
-    # Select a different book
-    # QUIT
-    # """
